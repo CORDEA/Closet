e349638ad3d03cf602910e8a8e4c97bd5d8820a6
Yoshihiro Tanaka <contact@cordea.jp>
2024-05-12 13:56:57 +0900

    update the layout

--- app/src/main/java/jp/cordea/closet/ui/home/Home.kt
+++ app/src/main/java/jp/cordea/closet/ui/home/Home.kt
@@ -1,17 +1,64 @@
 package jp.cordea.closet.ui.home
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun Home() {
-    Scaffold { padding ->
-        Text(
-            text = "Hello!",
-            modifier = Modifier.padding(padding)
-        )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Home")
+                }, actions = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = "Settings"
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { },
+                content = {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add"
+                    )
+                }
+            )
+        }
+    ) { padding ->
+        LazyColumn(modifier = Modifier.padding(padding)) {
+
+        }
     }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    Home()
 }
