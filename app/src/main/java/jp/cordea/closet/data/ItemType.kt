dfedc09298f3cb3c4a2b8b253112631c957f9bca
Yoshihiro Tanaka <contact@cordea.jp>
2024-05-19 11:52:57 +0900

    define the item type

---
+++ app/src/main/java/jp/cordea/closet/data/ItemType.kt
@@ -0,0 +1,15 @@
+package jp.cordea.closet.data
+
+enum class ItemType {
+    TOPS,
+    OUTERWEAR,
+    PANTS,
+    SKIRTS,
+    DRESSES,
+    SWIMWEAR,
+    SHOES,
+    BAGS,
+    HATS,
+    RINGS,
+    OTHERS
+}
