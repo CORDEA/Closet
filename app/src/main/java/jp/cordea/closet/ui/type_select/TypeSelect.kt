792ef951beeca5ca5ddb7cccbb1da94772d264b5
Yoshihiro Tanaka <contact@cordea.jp>
2024-05-19 12:06:19 +0900

    update the layout

--- app/src/main/java/jp/cordea/closet/ui/type_select/TypeSelect.kt
+++ app/src/main/java/jp/cordea/closet/ui/type_select/TypeSelect.kt
@@ -1,6 +1,10 @@
 package jp.cordea.closet.ui.type_select
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,9 +14,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import jp.cordea.closet.data.ItemType
@@ -20,17 +27,35 @@ import jp.cordea.closet.data.ItemType
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun TypeSelect() {
+    val behavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
                     Text(text = "Select")
-                }
+                },
+                scrollBehavior = behavior
             )
-        }
+        },
+        modifier = Modifier.nestedScroll(behavior.nestedScrollConnection)
     ) { padding ->
-        Box(modifier = Modifier.padding(padding)) {
-            LazyColumn(modifier = Modifier.padding(16.dp)) {
+        val layoutDirection = LocalLayoutDirection.current
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = padding.calculateTopPadding(),
+                    start = padding.calculateStartPadding(layoutDirection),
+                    end = padding.calculateEndPadding(layoutDirection),
+                )
+        ) {
+            LazyColumn(
+                contentPadding = PaddingValues(
+                    start = 24.dp,
+                    end = 24.dp,
+                    bottom = 32.dp
+                )
+            ) {
                 ItemType.entries.forEach { type ->
                     item { Item(type = type) }
                 }
@@ -50,7 +75,7 @@ private fun Item(type: ItemType) {
         Text(
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
-                .padding(12.dp),
+                .padding(16.dp),
             text = type.toString(),
             style = MaterialTheme.typography.headlineSmall
         )
