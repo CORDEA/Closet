7cc5e4d9c1fe16365a24a23287a5bc23ccfae190
Yoshihiro Tanaka <contact@cordea.jp>
2024-05-19 14:55:20 +0900

    define the item

--- app/src/main/java/jp/cordea/closet/ui/home/Home.kt
+++ app/src/main/java/jp/cordea/closet/ui/home/Home.kt
@@ -1,28 +1,46 @@
 package jp.cordea.closet.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import jp.cordea.closet.R
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun Home(navController: NavController) {
+    val behavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold(
         topBar = {
             TopAppBar(
@@ -38,7 +56,8 @@ fun Home(navController: NavController) {
                             contentDescription = "Settings"
                         )
                     }
-                }
+                },
+                scrollBehavior = behavior
             )
         },
         floatingActionButton = {
@@ -53,11 +72,67 @@ fun Home(navController: NavController) {
                     )
                 }
             )
-        }
+        },
+        modifier = Modifier.nestedScroll(behavior.nestedScrollConnection)
     ) { padding ->
-        LazyColumn(modifier = Modifier.padding(padding)) {
-
+        val layoutDirection = LocalLayoutDirection.current
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = padding.calculateTopPadding(),
+                    start = padding.calculateStartPadding(layoutDirection),
+                    end = padding.calculateEndPadding(layoutDirection),
+                )
+        ) {
+            LazyColumn(
+                contentPadding = PaddingValues(
+                    top = 16.dp,
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = 32.dp
+                )
+            ) {
+                item {
+                    Item()
+                }
+                item {
+                    Item()
+                }
+                item {
+                    Item()
+                }
+            }
         }
+    }
+}
+
+@Composable
+private fun Item() {
+    Card(
+        modifier = Modifier.padding(vertical = 8.dp),
+        onClick = { /*TODO*/ }
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(ratio = 19f / 10f)
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentScale = ContentScale.Crop,
+                contentDescription = "Thumbnail"
+            )
+            Text(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 12.dp)
+                    .align(Alignment.BottomStart),
+                text = "Item",
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+
     }
 }
 
