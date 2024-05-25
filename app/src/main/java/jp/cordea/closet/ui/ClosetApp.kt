9e2cddbedd2ff0a448a7e70bb9ef28e3d21f9fc3
Yoshihiro Tanaka <contact@cordea.jp>
2024-05-25 11:45:10 +0900

    add a new page

--- app/src/main/java/jp/cordea/closet/ui/ClosetApp.kt
+++ app/src/main/java/jp/cordea/closet/ui/ClosetApp.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.rememberNavController
 import jp.cordea.closet.data.ItemType
 import jp.cordea.closet.ui.add_item.AddItem
 import jp.cordea.closet.ui.home.Home
+import jp.cordea.closet.ui.item_details.ItemDetails
 import jp.cordea.closet.ui.type_select.TypeSelect
 
 @Composable
@@ -27,6 +28,11 @@ private fun AppNavHost(navController: NavHostController) {
         composable("add-item/{type}") {
             val type = requireNotNull(it.arguments?.getString("type"))
             AddItem(ItemType.valueOf(type))
+        }
+        composable("item-details/{id}") {
+            val id = requireNotNull(it.arguments?.getString("id"))
+            // TODO
+            ItemDetails(ItemType.OUTERWEAR)
         }
     }
 }
--- app/src/main/java/jp/cordea/closet/ui/item_details/ItemDetails.kt
+++ app/src/main/java/jp/cordea/closet/ui/item_details/ItemDetails.kt
@@ -1,22 +1,27 @@
-package jp.cordea.closet.ui.home
+package jp.cordea.closet.ui.item_details
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Card
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AssistChip
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -25,52 +30,41 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import jp.cordea.closet.R
+import jp.cordea.closet.data.ItemAttribute
+import jp.cordea.closet.data.ItemType
+import jp.cordea.closet.ui.toLocalizedString
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
-fun Home(navController: NavController) {
+fun ItemDetails(type: ItemType) {
     val behavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = "Home")
-                }, actions = {
+                    Text(text = "")
+                },
+                actions = {
                     IconButton(
                         onClick = {}
                     ) {
                         Icon(
                             modifier = Modifier.size(24.dp),
-                            imageVector = Icons.Default.Settings,
-                            contentDescription = "Settings"
+                            imageVector = Icons.Default.Edit,
+                            contentDescription = "Edit"
                         )
                     }
                 },
                 scrollBehavior = behavior
-            )
-        },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
-                    navController.navigate("type-select")
-                },
-                content = {
-                    Icon(
-                        imageVector = Icons.Default.Add,
-                        contentDescription = "Add"
-                    )
-                }
             )
         },
         modifier = Modifier.nestedScroll(behavior.nestedScrollConnection)
@@ -93,50 +87,89 @@ fun Home(navController: NavController) {
                     bottom = 32.dp
                 )
             ) {
+                item { Thumbnail() }
+                item { Spacer(modifier = Modifier.height(24.dp)) }
                 item {
-                    Item(navController)
+                    Item(ItemAttribute.SIZE)
                 }
+                content(type)
                 item {
-                    Item(navController)
+                    Item(ItemAttribute.MATERIAL)
                 }
+                item { Spacer(modifier = Modifier.height(16.dp)) }
+                item { Tag() }
             }
         }
     }
 }
 
-@Composable
-private fun Item(navController: NavController) {
-    Card(
-        modifier = Modifier.padding(vertical = 8.dp),
-        onClick = {
-            navController.navigate("item-details/id")
+private fun LazyListScope.content(type: ItemType) {
+    items(
+        count = type.attributes.size,
+        itemContent = {
+            Item(type.attributes.elementAt(it))
         }
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(ratio = 19f / 10f)
-        ) {
-            Image(
-                modifier = Modifier.fillMaxSize(),
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentScale = ContentScale.Crop,
-                contentDescription = "Thumbnail"
-            )
-            Text(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp, vertical = 12.dp)
-                    .align(Alignment.BottomStart),
-                text = "Item",
-                style = MaterialTheme.typography.headlineSmall
-            )
-        }
+    )
+}
 
+@Composable
+private fun Thumbnail() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(ratio = 19f / 10f)
+            .clip(RoundedCornerShape(percent = 16))
+    ) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentScale = ContentScale.Crop,
+            contentDescription = "Thumbnail"
+        )
     }
+}
+
+@Composable
+private fun Item(attribute: ItemAttribute) {
+    Column(modifier = Modifier.padding(vertical = 8.dp)) {
+        Text(
+            text = attribute.toLocalizedString(),
+            style = MaterialTheme.typography.bodySmall,
+        )
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(
+            modifier = Modifier.padding(start = 16.dp),
+            style = MaterialTheme.typography.bodyLarge,
+            text = "text"
+        )
+    }
+}
+
+@Composable
+@OptIn(ExperimentalLayoutApi::class)
+private fun Tag() {
+    FlowRow {
+        Chip()
+        Chip()
+        Chip()
+        Chip()
+        Chip()
+        Chip()
+        Chip()
+    }
+}
+
+@Composable
+private fun Chip() {
+    AssistChip(
+        modifier = Modifier.padding(horizontal = 4.dp),
+        onClick = { },
+        label = { Text(text = "text") }
+    )
 }
 
 @Preview
 @Composable
 private fun Preview() {
-    Home(rememberNavController())
+    ItemDetails(ItemType.OUTERWEAR)
 }
