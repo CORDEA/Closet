8df77cf7e78e29edb0c5dea68b4f384291ea7d5e
Yoshihiro Tanaka <contact@cordea.jp>
2024-05-25 12:09:03 +0900

    upgrade kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.jetbrains.kotlin.kapt)
     alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
