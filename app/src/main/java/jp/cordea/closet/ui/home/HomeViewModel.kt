b7ab553b00c848084b2911cd955416905b0f2697
Yoshihiro Tanaka <contact@cordea.jp>
2025-01-13 12:59:48 +0900

    resolve lint hints

--- app/src/main/java/jp/cordea/closet/ui/home/HomeViewModel.kt
+++ app/src/main/java/jp/cordea/closet/ui/home/HomeViewModel.kt
@@ -40,8 +40,8 @@ class HomeViewModel @Inject constructor(
         .flatMapLatest {
             flow {
                 emit(itemRepository.findAll())
-            }.map {
-                it.map {
+            }.map { items ->
+                items.map {
                     HomeItem(
                         it.id,
                         it.title,
