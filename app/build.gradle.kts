8d0e624795a88e9d56e7cb356be3366fcffd6c06
Yoshihiro Tanaka <contact@cordea.jp>
2025-01-13 12:57:21 +0900

    change the target SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ plugins {
 
 android {
     namespace = "jp.cordea.closet"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "jp.cordea.closet"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
